13b27cd9992a39653473a450d738fdf99f4df230
igorkcarneiro <igorcarneiros@pm.me>
2024-07-23 18:10:37 -0300

    v3.1 - added error handling messages; did some bug fixes, added a note for the --help flag

--- procontor.sh
+++ procontor.sh
@@ -20,6 +20,8 @@ show_help() {
     echo -e "${BLUE}  -s, --size SIZE${CLEAN}        Specify the container size (e.g., 500M, 5G)"
     echo -e "${WHITE}Description:${CLEAN}"
     echo -e "  Procontor is a Project Container Creator designed to create LUKS-encrypted containers for handling sensitive projects and subsequently mount them for further use. Please remember the password you've set for your container!"
+    echo -e "${WHITE}Note:${CLEAN}"
+    echo -e "  The ideal container naming should follow these rules: it should have the client's name followed by the date of the project start. For instance: evilcorp_14-02-24"
     exit 0
 }
 
@@ -27,18 +29,29 @@ if [ "$#" -eq 0 ]; then
     show_help
 fi
 
+project=""
+size=""
+
 while [[ "$#" -gt 0 ]]; do
     case "$1" in
         -h|--help)
             show_help
             ;;
         -p|--project)
-            project="$2"
-            shift 2
+            if [ -n "$2" ]; then
+                project="$2"
+                shift 2
+            else
+                error_exit "No project name provided after -p or --project."
+            fi
             ;;
         -s|--size)
-            size="$2"
-            shift 2
+            if [ -n "$2" ]; then
+                size="$2"
+                shift 2
+            else
+                error_exit "No size provided after -s or --size."
+            fi
             ;;
         *)
             echo -e "${RED}Unknown option: $1${CLEAN}"
@@ -47,30 +60,39 @@ while [[ "$#" -gt 0 ]]; do
     esac
 done
 
+# Verificar se os parâmetros obrigatórios foram fornecidos
+if [ -z "$project" ]; then
+    error_exit "Project name is required. Use -p or --project to specify it."
+fi
+
+if [ -z "$size" ]; then
+    error_exit "Container size is required. Use -s or --size to specify it."
+fi
+
+# Verificar se o valor após a flag -s é válido
+if [[ ! "$size" =~ ^[0-9]+[GgMm]$ ]]; then
+    error_exit "Invalid size format. Please use 'G' for gigabytes or 'M' for megabytes."
+fi
+
 if ! command -v cryptsetup &> /dev/null; then
     error_exit "cryptsetup is not installed. Please install it."
 fi
 
 create_container() {
-    echo -e "${BLUE}01 Creating a ${size} container for $project-cont${CLEAN}"
-    if [[ "$size" =~ ^[0-9]+[GgMm]$ ]]; then
-        dd if=/dev/zero of="$project-cont" bs=1 count=0 seek="$size"
-    else
-        echo -e "${RED}Invalid size format. Please use 'G' for gigabytes or 'M' for megabytes.${CLEAN}"
-        exit 1
-    fi
+    echo -e "${BLUE}01 Creating a ${size} container for $project${CLEAN}"
+    dd if=/dev/zero of="$project" bs=1 count=0 seek="$size" || error_exit "Failed to create the container."
     echo ""
 }
 
 encrypt_container() {
-    echo -e "${BLUE}02 Encrypting the $project-cont container with LUKS${CLEAN}"
-    sudo cryptsetup -y --cipher aes-xts-plain64 --hash sha512 --key-size 512 -v luksFormat "$project-cont" || error_exit "Failed to encrypt the container."
+    echo -e "${BLUE}02 Encrypting the $project container with LUKS${CLEAN}"
+    sudo cryptsetup -y --cipher aes-xts-plain64 --hash sha512 --key-size 512 -v luksFormat "$project" || error_exit "Failed to encrypt the container."
     echo ""
 }
 
 create_device() {
     echo -e "${BLUE}03 Creating the device as project-device${CLEAN}"
-    sudo cryptsetup luksOpen "$project-cont" project-device || error_exit "Failed to create the device."
+    sudo cryptsetup luksOpen "$project" project-device || error_exit "Failed to create the device."
     echo -e "${BLUE}The project-device is in /dev/mapper:${CLEAN}"
     ls /dev/mapper/
     echo ""
@@ -103,7 +125,7 @@ do
     echo -e " | .__/|_|  \___/ \___\___/|_| |_|\__\___/|_|   "
     echo -e " |_|                                            "
     echo ""
-    echo -e "     by k4rkarov (v3.0)"
+    echo -e "     by k4rkarov (v3.1)"
     echo ""
     echo ""
     create_container
@@ -118,3 +140,4 @@ do
     echo ""
     break
 done
+
